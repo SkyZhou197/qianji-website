af8cc5b542ca16ed61a809df86cf63bf79d106ef
skyzhou <zhouhaomin019@gmail.com>
2020-03-21 15:23:17 +0800

    deploy

---
+++ precache-manifest.6bd3e4ce73ed5f7590f7a1012a6fc2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4dc1143b13682942b0",
+    "url": "/qianji-website/css/app.60e13112.css"
+  },
+  {
+    "revision": "d12714040b3d621188085841bf0aad52",
+    "url": "/qianji-website/index.html"
+  },
+  {
+    "revision": "9b4dc1143b13682942b0",
+    "url": "/qianji-website/js/app.66872096.js"
+  },
+  {
+    "revision": "adae749461ca52bd8da0",
+    "url": "/qianji-website/js/chunk-vendors.aade821b.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/qianji-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qianji-website/robots.txt"
+  }
+]);
